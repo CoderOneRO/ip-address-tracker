9b009623888fc8cf9ec7aec5855c0d3cd9a84d24
Szabolcs Küsmödi <szabolcskusmodi@gmail.com>
2022-01-22 22:06:20 +0200

    leaflet implementation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
 function App() {
   return (
     <div className="App">
-      <h1>IP Address Tracker</h1>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true} height="100px">
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
