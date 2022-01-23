a5f17925e60866a31322819ad5d479ebd357352c
Szabolcs Küsmödi <szabolcskusmodi@gmail.com>
2022-01-23 14:09:34 +0200

    Details component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Header from './Header';
 import Map from './Map';
+import Details from './Details';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Map />
+      <Details />
     </div>
   );
 }
---
+++ src/Details.js
@@ -0,0 +1,22 @@
+function Details() {
+    return (
+        <div className="details-container">
+            <div className="details">
+                <div>
+                    <h1>Details</h1>
+                </div>
+                <div>
+                    <h1>Details</h1>
+                </div>
+                <div>
+                    <h1>Details</h1>
+                </div>
+                <div>
+                    <h1>Details</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Details;
