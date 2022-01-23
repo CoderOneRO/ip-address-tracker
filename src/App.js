936adf42495aab736bec3357e693771d664963be
Szabolcs Küsmödi <szabolcskusmodi@gmail.com>
2022-01-24 01:30:02 +0200

    show result on submit

--- src/App.js
+++ src/App.js
@@ -6,32 +6,27 @@ import Details from './Details';
 
 function App() {
 
-  const [details, setDetails] = useState([]);
   const [ip, setIp] = useState('');
   const [location, setLocation] = useState('');
   const [timezone, setTimezone] = useState('');
   const [isp, setIsp] = useState('');
 
   const [searched, setSearched] = useState('');
+  const [search, setSearch] = useState('');
+
+  const URL = 'https://geo.ipify.org/api/v2/country?apiKey=at_UY6Hq4CmUxKMxfQAzcj3MgXfaWwC8&ipAddress=';
 
   useEffect(() => {
     const fetchData = () => {
-      fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_UY6Hq4CmUxKMxfQAzcj3MgXfaWwC8&ipAddress=${searched}`)
+      fetch(`${URL}${search}`)
         .then(response => response.json())
         .then(data => setDetailValues(data))
         .catch(err => console.log(err))
     }
     fetchData();
-  }, [searched])
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(e);
-    setSearched(e);
-  }
+  }, [search])
 
   const setDetailValues = (data) => {
-      setDetails(data)
       setIp(data.ip);
       setTimezone(data.location.timezone);
       setIsp(data.isp)
@@ -40,11 +35,10 @@ function App() {
 
   return (
     <div className="App">
-      {console.log(details)}
       <Header
         searched={searched}
         setSearched={setSearched}
-        handleSubmit={handleSubmit}
+        setSearch={setSearch}
       />
       <Map />
       <Details
--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,15 @@
-function Header({ searched, setSearched, handleSubmit }) {
+function Header({ searched, setSearched, setSearch }) {
 
-    const handleChange = (e) => {
-        setSearched(e)
+    const handleSubmit = (e) => {
+        setSearch(searched);
+        e.preventDefault();
     }
 
     return (
         <div className="header">
             <h1>IP Address Tracker</h1>
-            <form onSubmit={handleSubmit}>
-                <input type="text" className="input-text" onChange={(e) => handleChange(e.target.value)} placeholder="Search for any IP address" />
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <input type="text" className="input-text" value={searched} onChange={(e) => setSearched(e.target.value)} placeholder="Search for any IP address" />
                 <input type="submit" value=">" className="input-submit" />
             </form>
         </div>
